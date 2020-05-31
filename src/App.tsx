aecac8666fb766a98f4978936555bef0ebe1a2b7
Tsukasa Sekiguchi <t14159265358979323846@gmail.com>
2020-05-31 21:55:07 +0900

    add to ionic

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  IonApp,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton,
+} from "@ionic/react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <IonApp>
+        <IonContent>
+          <IonHeader>
+            <IonToolbar>
+              <IonTitle>IonicReactApp</IonTitle>
+            </IonToolbar>
+          </IonHeader>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <IonButton href="https://reactjs.org">Learn React</IonButton>
+        </IonContent>
+      </IonApp>
     </div>
   );
 }
